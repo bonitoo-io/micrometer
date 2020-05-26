6cc9f17b96528174b25b16264e4a76373f50b628
Jakub Bednar <jakub.bednar@gmail.com>
2020-05-26 08:28:14 +0200

    feat: add workaround for InfluxDB v2

--- samples/micrometer-samples-boot2/src/main/java/io/micrometer/boot2/samples/InfluxSample.java
+++ samples/micrometer-samples-boot2/src/main/java/io/micrometer/boot2/samples/InfluxSample.java
@@ -16,8 +16,17 @@
 package io.micrometer.boot2.samples;
 
 import io.micrometer.boot2.samples.components.PersonController;
+import io.micrometer.core.instrument.Clock;
+import io.micrometer.core.ipc.http.OkHttpSender;
+import io.micrometer.influx.InfluxConfig;
+import io.micrometer.influx.InfluxMeterRegistry;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackageClasses = PersonController.class)
@@ -25,5 +34,51 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class InfluxSample {
     public static void main(String[] args) {
         new SpringApplicationBuilder(InfluxSample.class).profiles("influx").run(args);
+    }
+
+    @Value("${management.metrics.export.influx.org}")
+    private String org;
+
+    @Value("${management.metrics.export.influx.bucket}")
+    private String bucket;
+
+    @Value("${management.metrics.export.influx.token}")
+    private String token;
+
+    @Bean
+    public InfluxMeterRegistry influxMeterRegistry(InfluxConfig influxConfig, Clock clock) {
+
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+
+        httpClient.addInterceptor(chain -> {
+            Request original = chain.request();
+
+            // skip others than write
+            if (!original.url().pathSegments().contains("write")) {
+                 return  chain.proceed(original);
+            }
+
+            HttpUrl url = original.url()
+                    .newBuilder()
+                    .removePathSegment(0)
+                    .addEncodedPathSegments("api/v2/write")
+                    .removeAllQueryParameters("db")
+                    .removeAllQueryParameters("consistency")
+                    .addQueryParameter("org", org)
+                    .addQueryParameter("bucket", bucket)
+                    .build();
+
+            Request request = original.newBuilder()
+                    .url(url)
+                    .header("Authorization", "Token " + token)
+                    .build();
+
+            return chain.proceed(request);
+        });
+
+        return InfluxMeterRegistry.builder(influxConfig)
+                .clock(clock)
+                .httpClient(new OkHttpSender(httpClient.build()))
+                .build();
     }
 }
